9a2aa0952df4d683a0ba3058305a37f09aecfca2
amirzenoozi <amirzenoozi72@gmail.com>
2024-06-03 14:03:55 +0200

    :white_check_mark: Fix: Update Tests

--- src/components/radio-group/index.test.tsx
+++ src/components/radio-group/index.test.tsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import RadioGroup from './index'
-import metricOptions from '../../constants/metricOptions'
+
+const radioOptions = [
+	{
+		value: 'all',
+		label: 'All'
+	},
+	{
+		value: 'yes',
+		label: 'Yes'
+	},
+	{
+		value: 'no',
+		label: 'No'
+	}
+]
 
 describe('RadioGroup', () => {
 	test('Single Radio', () => {
-		render(<RadioGroup options={[metricOptions[0]]} name={'metric'} value={'all'} clickHandler={(msg) => { console.log(msg) }} />)
+		render(<RadioGroup options={[radioOptions[0]]} name={'metric'} value={'all'} clickHandler={(msg) => { console.log(msg) }} />)
 		const linkElement = screen.getByLabelText(/All/i)
 		expect(linkElement).toBeInTheDocument()
 	})
 
 	test('Multiple Radio', () => {
-		render(<RadioGroup options={metricOptions} name={'metric'} value={'all'} clickHandler={(msg) => { console.log(msg) }} />)
+		render(<RadioGroup options={radioOptions} name={'metric'} value={'all'} clickHandler={(msg) => { console.log(msg) }} />)
 		const allLabel = screen.getByLabelText(/All/i)
 		const cpuLabel = screen.getByLabelText(/CPU/i)
 		const memoryLabel = screen.getByLabelText(/Memory/i)
